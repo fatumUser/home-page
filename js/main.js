2ea1cf3a7f202b9a2726df1d4a955fd704cf3073
fatumUser <129615173+fatumUser@users.noreply.github.com>
2023-09-20 19:48:03 +0300

    f

---
+++ js/main.js
@@ -0,0 +1,80 @@
+function dropBlock() {
+  document.getElementById("drop-block").classList.toggle("drop-block-show");
+  document
+    .getElementById("text_special-offers")
+    .classList.toggle("text_special-offers-show");
+}
+function footerMenuCoupons() {
+  document
+    .getElementById("footer-col__title-1")
+    .classList.toggle("footer-col__title-show");
+}
+function footerMenuSocial() {
+  document
+    .getElementById("footer-col__title-2")
+    .classList.toggle("footer-col__title-show");
+}
+function imgPlusMinus() {
+  document
+    .getElementById("accordeon-block__img-plus")
+    .classList.toggle("accordeon-block__img-plus-none");
+  document
+    .getElementById("accordeon-block__img-minus")
+    .classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_2() {
+  document
+    .getElementById("accordeon-block__img-plus_2")
+    .classList.toggle("accordeon-block__img-plus-none");
+  document
+    .getElementById("accordeon-block__img-minus_2")
+    .classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_3() {
+  document
+    .getElementById("accordeon-block__img-plus_3")
+    .classList.toggle("accordeon-block__img-plus-none");
+  document
+    .getElementById("accordeon-block__img-minus_3")
+    .classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_4() {
+  document
+    .getElementById("accordeon-block__img-plus_4")
+    .classList.toggle("accordeon-block__img-plus-none");
+  document
+    .getElementById("accordeon-block__img-minus_4")
+    .classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_5() {
+  document
+    .getElementById("accordeon-block__img-plus_5")
+    .classList.toggle("accordeon-block__img-plus-none");
+  document
+    .getElementById("accordeon-block__img-minus_5")
+    .classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_6() {
+  document
+    .getElementById("accordeon-block__img-plus_6")
+    .classList.toggle("accordeon-block__img-plus-none");
+  document
+    .getElementById("accordeon-block__img-minus_6")
+    .classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_7() {
+  document
+    .getElementById("accordeon-block__img-plus_7")
+    .classList.toggle("accordeon-block__img-plus-none");
+  document
+    .getElementById("accordeon-block__img-minus_7")
+    .classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_8() {
+  document.getElementById("accordeon-block__img-plus_8").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_8").classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_9() {
+  document.getElementById("accordeon-block__img-plus_9").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_9").classList.toggle("accordeon-block__img-minus-show");
+}
